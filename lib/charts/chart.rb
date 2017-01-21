928ad9eefb92cfe19e341fcca437e18af52e7b67
kotp <keeperotphones@gmail.com>
2017-01-21 16:05:39 -0500

    Orientation to the chart
    Add a restriction to the interface exposure for Chart, by making most
    of the methods private.
    
    Remove the spec files from rubocop considerations for now

--- lib/charts/chart.rb
+++ lib/charts/chart.rb
@@ -1,5 +1,70 @@
+module Validable
+  private
+
+  def validate_arguments(data, options)
+    validate_options options
+    validate_data data
+    array?(options, [:labels, :colors])
+    validate_colors(data, options, :colors)
+    validate_labels(data, options, :labels)
+  end
+
+  # rubocop:disable Style/AndOr
+  def array?(options, keys)
+    keys.each do |key|
+      options[key] and
+        options[key].is_a?(Array) ||
+          raise(ArgumentError, "#{key} not an array")
+    end
+  end
+
+  def validate_options(options)
+    options.is_a?(Hash) or raise ArgumentError, 'Options missing'
+    options[:outer_margin].nil? or options[:outer_margin].is_a?(Numeric) or
+      raise(ArgumentError, 'outer_margin not a number')
+  end
+
+  def validate_data(data)
+    data.empty? and raise(ArgumentError, 'Data missing')
+    data.is_a?(Array) or raise ArgumentError, 'Data not an array'
+  end # rubocop:enable Style/AndOr
+
+  def validate_colors(data, options, key)
+    options[key] &&
+      options[key].any? && data.count > options[key].count &&
+      raise(ArgumentError, "number of #{key} is too small")
+  end
+
+  def validate_labels(data, options, key)
+    options[key] &&
+      options[key].any? && (data.count != options[key].count) &&
+      raise(ArgumentError, "number of #{key} does not match array")
+  end
+end
+
 class Charts::Chart
+  include Validable
+
   attr_reader :data, :options, :prepared_data, :renderer
+
+  OPTIONS = { # rubocop:disable Style/MutableConstant
+    title:            nil,
+    type:             :svg,
+    outer_margin:     30,
+    background_color: 'white',
+    labels:           [],
+    colors:           [
+      '#e41a1d',
+      '#377eb9',
+      '#4daf4b',
+      '#984ea4',
+      '#ff7f01',
+      '#ffff34',
+      '#a65629',
+      '#f781c0',
+      '#888888'
+    ]
+  }
 
   def initialize(data, opts = {})
     validate_arguments(data, opts)
@@ -10,93 +75,31 @@ class Charts::Chart
     @prepared_data = prepare_data
   end
 
-  def validate_arguments(data, options)
-    raise ArgumentError, 'Data missing' if data.empty?
-    raise ArgumentError, 'Data not an array' unless data.is_a? Array
-    raise ArgumentError, 'Options missing' unless options.is_a? Hash
-    if options[:outer_margin] && !options[:outer_margin].is_a?(Numeric)
-      raise ArgumentError, 'outer_margin not a number'
-    end
-    array?(options, [:labels, :colors])
-    validate_colors(data, options, :colors)
-    validate_labels(data, options, :labels)
-  end
-
-  def array?(options, keys)
-    keys.map do |key|
-      next unless options[key]
-      unless options[key].is_a?(Array)
-        raise ArgumentError, "#{key} not an array"
-      end
-    end
-  end
-
-  def validate_colors(data, options, key)
-    if options[key]
-      if options[key].any? && data.count > options[key].count
-        raise ArgumentError, "number of #{key} is too small"
-      end
-    end
-  end
-
-  def validate_labels(data, options, key)
-    if options[key]
-      if options[key].any? && (data.count != options[key].count)
-        raise ArgumentError, "number of #{key} does not match array"
-      end
-    end
-  end
-
-  def default_options
-    {
-      title:            nil,
-      type:             :svg,
-      outer_margin:     30,
-      background_color: 'white',
-      labels:           [],
-      colors:           [
-        '#e41a1d',
-        '#377eb9',
-        '#4daf4b',
-        '#984ea4',
-        '#ff7f01',
-        '#ffff34',
-        '#a65629',
-        '#f781c0',
-        '#888888'
-      ]
-    }
-  end
-
-  def prepare_data
-    data
-  end
-
   def render
     pre_draw
     draw
     post_draw
   end
 
-  def pre_draw
-    @renderer = Charts::Renderer.new(self)
-    draw_background
-    draw_title
-  end
-
   def draw
     raise NotImplementedError
   end
 
-  def post_draw
-    renderer.post_draw
+  private
+
+  def default_options
+    OPTIONS
   end
 
   def draw_background
-    renderer.rect 0, 0, width, height, fill: background_color, class: 'background_color'
+    renderer.rect(
+      0, 0, width, height,
+      fill:  background_color,
+      class: 'background_color'
+    )
   end
 
-  def draw_title
+  def draw_title # rubocop:disable Metrics/AbcSize
     return unless options[:title]
     x = width / 2
     y = outer_margin / 2 + 2 * renderer.font_size / 5
@@ -110,5 +113,19 @@ class Charts::Chart
     options.each do |key, value|
       define_singleton_method key, proc { value }
     end
+  end
+
+  def pre_draw
+    @renderer = Charts::Renderer.new(self)
+    draw_background
+    draw_title
+  end
+
+  def post_draw
+    renderer.post_draw
+  end
+
+  def prepare_data
+    data
   end
 end
